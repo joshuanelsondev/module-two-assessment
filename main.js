8bbfd4255fbfba197afb4a6611d5614be415a7ee
joshuanelsondev <joshuanelsondev@gmail.com>
2023-01-18 13:06:09 -0500

    Create function for compiling Movie data

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
+const BASE_URL = 'https://resource-ghibli-api.onrender.com/films';
+const movieArr = [];
 
 
 
 
 
 
+function compileMovieData(response) {
+    response.forEach(movieObj => {
+        const currentMovieObj = {
+            id: movieObj.id, 
+            title: movieObj.title,
+            released: movieObj.release_date,
+            description: movieObj.description,
+            people: movieObj.people
+        };
+        movieArr.push(currentMovieObj);
+    });
+}
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+    fetch(BASE_URL)
+        .then((response) => response.json())
+        .then((response) => compileMovieData(response))
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
